8afeab9f02671a155a4128dc5419b1d86582262d
Bartłomiej Rutkowski <bartek@ethworks.io>
2018-09-03 15:21:38 +0200

    minor fix

--- test/services/validations.js
+++ test/services/validations.js
@@ -35,20 +35,20 @@ describe('Validations', () => {
     };
 
     it('returns true for valid private key', async () => {
-      await expect(validations.isValidPrivateKey(inputs.correct)).to.eventually.be.true;
+      expect(validations.isValidPrivateKey(inputs.correct)).to.be.true;
     });
 
     it('returns false if private key has wrong length', async () => {
-      await expect(validations.isValidPrivateKey(inputs.tooShort)).to.eventually.be.false;
-      await expect(validations.isValidPrivateKey(inputs.tooLong)).to.eventually.be.false;
+      expect(validations.isValidPrivateKey(inputs.tooShort)).to.be.false;
+      expect(validations.isValidPrivateKey(inputs.tooLong)).to.be.false;
     });
 
     it('returns false if private key has no 0x prefix', async () => {
-      await expect(validations.isValidPrivateKey(inputs.noPrefix)).to.eventually.be.false;
+      expect(validations.isValidPrivateKey(inputs.noPrefix)).to.be.false;
     });
 
     it('returns false if private key is not a hex value', async () => {
-      await expect(validations.isValidPrivateKey(inputs.notHex)).to.eventually.be.false;
+      expect(validations.isValidPrivateKey(inputs.notHex)).to.be.false;
     });
   });
 
@@ -64,7 +64,7 @@ describe('Validations', () => {
 
     correctInputs.forEach((url) => {
       it(`returns true for ${url}`, async () => {
-        await expect(validations.isValidUrl(url)).to.eventually.be.true;
+        expect(validations.isValidUrl(url)).to.be.true;
       });
     });
 
@@ -80,7 +80,7 @@ describe('Validations', () => {
 
     incorrectInputs.forEach((url) => {
       it(`returns false for ${url}`, async () => {
-        await expect(validations.isValidUrl(url)).to.eventually.be.false;
+        expect(validations.isValidUrl(url)).to.be.false;
       });
     });
   });
@@ -100,7 +100,7 @@ describe('Validations', () => {
 
     correctInputs.forEach((email) => {
       it(`returns true for ${email}`, async () => {
-        await expect(validations.isValidEmail(email)).to.eventually.be.true;
+        expect(validations.isValidEmail(email)).to.be.true;
       });
     });
 
@@ -121,7 +121,7 @@ describe('Validations', () => {
 
     incorrectInputs.forEach((email) => {
       it(`returns false for ${email}`, async () => {
-        await expect(validations.isValidEmail(email)).to.eventually.be.false;
+        expect(validations.isValidEmail(email)).to.be.false;
       });
     });
   });
