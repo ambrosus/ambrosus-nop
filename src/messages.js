eb7f7c412e9b6c977b4eb46c1439553fbc72bd6c
Bartłomiej Rutkowski <bartek@ethworks.io>
2018-09-03 14:43:46 +0200

    minor improvement

--- src/messages.js
+++ src/messages.js
@@ -28,8 +28,8 @@ const messages = {
   nodeUrlInputInstruction: 'Please provide URL, which you will be using for your node:',
   nodeUrlInputError: 'The provided value is not a valid URL',
   nodeUrlInfo: 'Node URL defined as ',
-  userEmailInputInstruction: 'Please provide your e-mail address:',
-  userEmailInputError: 'The provided value is not a valid e-mail address',
+  userEmailInputInstruction: 'Please provide your email address:',
+  userEmailInputError: 'The provided value is not a valid email address',
   userEmailInfo: 'Your email address is '
 };
 
