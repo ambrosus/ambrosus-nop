a80e560c8f01edfc1a3d0e39ef0855f16f7fc897
Ivan Rukhavets <ivanruch@gmail.com>
2019-05-15 11:59:29 +0200

    Add docker-compose pull in update script (#82)

--- update.sh
+++ update.sh
@@ -5,3 +5,7 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 git pull origin master
 yarn
 yarn build
+if [[ -f output/docker-compose.yml ]]; then
+  yarn start update
+  docker-compose -f output/docker-compose.yml pull
+fi
