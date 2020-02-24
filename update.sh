721437c44ae8cd08115a02018302cf92dbad533c
Oleksiy Pyltsov <valar999@gmail.com>
2020-02-25 01:06:47 +0200

    Run "docker-compose up" in update.sh

--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ yarn
 yarn build
 if [[ -f output/docker-compose.yml ]]; then
   yarn start update
-  docker-compose -f output/docker-compose.yml pull
+  docker-compose -f output/docker-compose.yml up -d
 fi
