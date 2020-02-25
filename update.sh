0397a3da5b5a272ae79271f02149af701706eae7
Oleksiy Pyltsov <valar999@gmail.com>
2020-02-25 10:24:48 +0200

    Add autoupdate (#128)
    * Run "docker-compose up" in update.sh
    
    * Add crontab job to run update.sh

--- update.sh
+++ update.sh
@@ -2,10 +2,18 @@
 set -ex
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
+if [[ -d /etc/cron.daily ]]; then
+  cronfile=/etc/cron.daily/ambrorus-nop
+  cat > $cronfile <<-END
+        #!/bin/sh
+        ~/ambrosus-nop/update.sh
+        END
+  chmod +x $cronfile
+fi
 git pull origin master
 yarn
 yarn build
 if [[ -f output/docker-compose.yml ]]; then
   yarn start update
-  docker-compose -f output/docker-compose.yml pull
+  docker-compose -f output/docker-compose.yml up -d
 fi
