4d69a9d1fd89ae3f5fe8c7f27aefeeef9e1b4a48
Andrii Rozinko <31214139+AndrewAR2@users.noreply.github.com>
2022-05-06 14:24:53 +0200

    Fix folder position (#204)

--- run-update.sh
+++ run-update.sh
@@ -33,6 +33,7 @@ if [ 16 -gt $BLOCKHAIN_DB_VERSION ] &&  [ "$NETWORK_ENV" == "ambnet-test" ]; the
     cd 3.1-db-upgrade-tool
     docker container stop parity
     echo "I AGREE" | cargo run ../$BLOCKHAIN_DB_UPDATE_PATH
+    cd ..
     rm -rf 3.1-db-upgrade-tool
 fi
 
