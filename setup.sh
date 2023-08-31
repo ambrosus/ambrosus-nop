6a1e905ff6dafe8b599dfa52f2067acb7bde0936
Eshanchik <39962460+Eshanchik@users.noreply.github.com>
2023-08-31 16:37:08 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 apt-get update
-apt install -y curl
-apt install -y python-dev
+apt-get install -y curl
+apt-get install -y python-dev
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 apt-get install -y build-essential
 apt-get install -y nodejs
-apt install -y npm
-apt install -y git
-apt install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y npm
+apt-get install -y git
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
@@ -21,7 +21,7 @@ apt-get install -y docker-ce docker-ce-cli containerd.io
 curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-apt install -y jq
+apt-get install -y jq
 npm install -g yarn
 
 # todo remove branch simplify-nop
