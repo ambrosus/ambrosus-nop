115fee3395c94dd2e3cc3efa965f4a22e3746410
Eshanchik <39962460+Eshanchik@users.noreply.github.com>
2023-08-31 22:25:11 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,28 +2,31 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt update -y
-apt upgrade -y
-
-apt-get install -y curl python-dev build-essential nodejs npm git apt-transport-https ca-certificates curl software-properties-common jq
-
+apt-get update -y
+apt-get install -y curl
+apt-get install -y python-dev
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+apt-get install -y build-essential
+apt-get install -y nodejs
+apt-get install -y npm
+apt-get install -y git
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o -y /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt update -y
-apt upgrade -y
-
+apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
-
 curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
+apt-get install -y jq
 npm install -g yarn
 
+# todo remove branch simplify-nop
 git clone -b simplify-nop https://github.com/ambrosus/ambrosus-nop.git
 cd ambrosus-nop || return
 
